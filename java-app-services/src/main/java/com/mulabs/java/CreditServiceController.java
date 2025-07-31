97f92bb5f26fb56aad53b7c0b913638c9fd28cd7
Mukund Babbar <mbabbar@splunk.com>
2025-07-31 19:43:42 +1000

    add port env variable

--- java-app-services/src/main/java/com/mulabs/java/CreditServiceController.java
+++ java-app-services/src/main/java/com/mulabs/java/CreditServiceController.java
@@ -18,30 +18,35 @@ public class CreditServiceController extends BaseController {
 
 	@RequestMapping("/pgp")
 	public String pgp(HttpServletRequest request) {
-		makeWebRequest(getTargetHost(), "8080", "AddressCheck/pgp", request);
+		makeWebRequest(getTargetHost(), getTargetPort(), "AddressCheck/pgp", request);
 		return "Hello from pgp";
 	}
 
 	@RequestMapping("/jg")
 	public String jg(HttpServletRequest request) {
-		makeWebRequest(getTargetHost(), "8080", "AddressCheck/jg", request);
+		makeWebRequest(getTargetHost(), getTargetPort(), "AddressCheck/jg", request);
 		return "Hello from jg";
 	}
 
 	@RequestMapping("/ua")
 	public String ua(HttpServletRequest request) {
-		makeWebRequest(getTargetHost(), "8080", "AddressCheck/ua", request);
+		makeWebRequest(getTargetHost(), getTargetPort(), "AddressCheck/ua", request);
 		return "Hello from ua";
 	}
 
 	@RequestMapping("/gw")
 	public String gw(HttpServletRequest request) {
-		makeWebRequest(getTargetHost(), "8080", "AddressCheck/gw", request);
+		makeWebRequest(getTargetHost(), getTargetPort(), "AddressCheck/gw", request);
 		return "Hello from gw";
 	}
 	
 	private String getTargetHost() {
 	    String envHost = System.getenv("EXTERNAL_ADDRESS_CHECK_URL");
 	    return (envHost != null && !envHost.isEmpty()) ? envHost : "address-check";
+	}
+	
+	private String getTargetPort() {
+	    String envPort = System.getenv("EXTERNAL_ADDRESS_CHECK_PORT");
+	    return (envPort != null && !envPort.isEmpty()) ? envPort : "8080";
 	}
 }
